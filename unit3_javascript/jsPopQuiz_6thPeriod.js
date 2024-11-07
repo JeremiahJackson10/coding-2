b465d7210cbd078e30ac08058ff7f913c2c76829
JeremiahJackson10 <swaze722@gmail.com>
2024-11-07 18:40:20 +0000

    pop quiz a little bit of done

---
+++ unit3_javascript/jsPopQuiz_6thPeriod.js
@@ -0,0 +1,58 @@
+// JavaScript Pop Quiz
+// Create a new document called jsPopQuiz.js and answer the following questions.
+// When you have completed the quiz, please commit and sync your work so that it can be
+// graded.
+
+// RULES
+// You are ONLY permitted to use W3 schools. 
+// You may NOT use any other external websites.
+// No music is permitted during the quiz. 
+// All phones must be put away- If a phone is visible, you will recieve a verbal warning.
+// A second warning will result in an automatic failure. 
+// Once you've completed your quiz, you may use the computer at your leisure- PLEASE DO NOT
+// disrupt/ distract your classmates who are still taking the quiz. 
+ 
+// 1. How do you declare a variable in JavaScript? Please write your answer 
+// using JavaScript code. 
+
+Automatically
+Using var
+Using let
+Using const
+
+// 2. How would you declare a variable where the data WILL NOT change in JavaScript?
+// Please write your answer using JavaScript code. 
+const constantvariable= 'constantValue'
+
+// 3. Identify the following JavaScript operators and describe when you would use it?
+// Please write your answers using the string data type. 
+//  === comparing values for strict equality
+//  ==equality with type coercion
+//  = 
+
+// 4. Describe a scenario where you would use a comparison operator,
+// and then provide a code example based on the scenerio you provided. 
+(userage>18){console.log('user is an adult');}
+// 5. Describe a scenario where you would use a logical operator,
+// and then provide a code example based on the scenerio you provided. 
+(number>= 10 && )
+// 6. Create three variables where when combined will result in the value of 36. 
+// Please use the console.log () to out put your result. 
+
+// 7. Fill in the variable that will output the appropriate result. 
+
+var x ='Please replace this text with your answer.'
+var y = 14*2
+
+// Your console log should output the value 28.
+console.log(y)
+
+// 8. What does it "concatenation" mean? Please write your response as a string
+// and also provide a code example of two variables being concatenated and output the result
+// using the console.log() function. 
+
+add strings together 
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)

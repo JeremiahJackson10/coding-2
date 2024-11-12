c459465f39fcb8adbdefc2d753e6b4d8f1d80238
JeremiahJackson10 <swaze722@gmail.com>
2024-11-12 18:40:31 +0000

    async work completed

---
+++ unit3_javascript/asyncactivity_nov8.js
@@ -0,0 +1,35 @@
+#1
+'String concatenation is when you combine two or more strings with'
+'the + (addition symbol) operator'
+a = 'this is'
+b = 'coding class'
+c = a + b
+#2
+'const - is used to declare variables that will not change'
+'let - is used to declare variables that will change'
+'var - universal/ defualt variable can be used in any context'
+
+'1. variable names can only start with letters'
+'2. variables names cannot have any white space'
+'3. variable names cannot be resereved words/ keywords'
+#3 
+- Camel case - Every word after the first word is capitalized 
+var username =''
+var timeRemainingInQtr= ''
+
+- snake case- every word is separated by underscore
+var user_Name =''
+var time_remaining_in_qtr= ''
+
+- Pascal case- EVERY WORD BEGINS WITH CAPITAL LETTER
+var User_Name =''
+var Time_Remaining_In_Qtr= ''
+#4
+let a = 350
+let b = a * 2
+console.log(b)
+#5
+x = 10
+y = 2
+z= y*2
+console.log( x > 1 && z==4)

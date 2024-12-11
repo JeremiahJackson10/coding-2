bc4c54145aeb4a1306243b9257402ffa24a77d20
JeremiahJackson10 <swaze722@gmail.com>
2024-12-11 18:31:52 +0000

    Programming grade function

---
+++ unit3_javascript/jsCondtionalReview.js
@@ -0,0 +1,40 @@
+//function is set of instructions that informs the computer on what to do with DataTransfer
+
+//conditional statement use the if/ else keywords to allow us to create multiple custom outcomes 
+
+
+
+function timeOfDay(time){
+   if(time == "am") {
+    console.log("it is the morning. ");
+   }else if (time == "pm"){
+    console.log("it is the afternoon. ");
+   }else {
+    console.log("sorry, cannot understand input");
+   }
+}
+
+timeOfDay()
+
+
+//create a function that will give
+//someone a letter grade based on their 
+//numerical grade score. for example 
+//if someone has a 90, your program 
+//should print you have an A, 
+//if the data entered is an 80 
+//the program should print you have a B, 
+//etc
+
+
+function GradeScale(time){
+    if(Grade 50% == "F") {
+     console.log("Your grade is an F. ");
+    }else if (Grade 60% == "D"){
+     console.log("Your grade is an D. ");
+    }else {
+     console.log("Your grade is an D");
+    }
+ }
+ 
+ GradeScale()

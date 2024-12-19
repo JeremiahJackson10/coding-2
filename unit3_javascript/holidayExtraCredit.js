4c1204fa23f73025b265ef6fdee13d52d1da397d
JeremiahJackson10 <swaze722@gmail.com>
2024-12-19 18:36:39 +0000

    holiday extra credit completed

--- unit3_javascript/holidayExtraCredit.js
+++ unit3_javascript/holidayExtraCredit.js
@@ -2,12 +2,12 @@
 function holidaySchedule(dateInput) {
     // Array with the holiday events between 12/21/2024 and 1/7/2025
     const events = {
-        "12/21/2024": "on Saturday Dec. 21, I'm going to lay down.",
-        "12/22/2024": "on Sunday Dec. 22, I'm making chicken.",
-        "12/23/2024": "on Monday Dec. 23, I'm going shopping.",
+        "12/21/2024": "on Saturday Dec. 21, I'm going to chill.",
+        "12/22/2024": "on Sunday Dec. 22, I'm baking cookies.",
+        "12/23/2024": "on Monday Dec. 23, I'm doing some last-minute shopping.",
         "12/24/2024": "on Tuesday Dec. 24, I'm having a Christmas Eve dinner.",
         "12/25/2024": "on Wednesday Dec. 25, I'm celebrating Christmas.",
-        "12/26/2024": "on Thursday Dec. 26, I'm relaxing with cousins.",
+        "12/26/2024": "on Thursday Dec. 26, I'm relaxing with family.",
         "12/31/2024": "on Tuesday Dec. 31, I'm going to a New Year's party.",
         "1/1/2025": "on Wednesday Jan. 1, I'm celebrating New Year's Day.",
         "1/2/2025": "on Thursday Jan. 2, I'm starting a new hobby.",

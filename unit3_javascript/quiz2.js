fcec73fc60bafd66f94037e14f6d7363ec02b77d
JeremiahJackson10 <swaze722@gmail.com>
2025-01-08 18:40:30 +0000

    quiz almost finished

---
+++ unit3_javascript/quiz2.js
@@ -0,0 +1,8 @@
+let question 1 = "Integer data types are used to store a whole number without decimals, like 35, -50, or 1345000";
+let question 2 = "Use else if to specify a new condition to test, if the first condition is false";
+let question 3 = "block of code that does a exact task";
+let question 4 = " mostly used for conditional testing including yes, no, true, or/and false";
+let question 5 = "Function parameters are the names listed in the function definition and Function arguments are the real values passed to (and received by) the function";
+let question 6 = " addition operator";
+let question 7 = "comparison operator";
+let question 8 = " ";

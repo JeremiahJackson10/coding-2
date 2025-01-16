f5cb04faa6a12685ab6321cb91277932af5ee667
JeremiahJackson10 <swaze722@gmail.com>
2025-01-16 18:21:27 +0000

    Notes completed

---
+++ jsArrayMethods.js
@@ -0,0 +1,23 @@
+classes = ["math",
+    "reading",
+    "gym",
+    10,
+    10.23,
+    true,
+    "reading"
+];
+
+var shirts['s','m','l]
+var shoe =['nike','addidas']
+
+// methods a special type of function that works on lists and objects
+
+//push() method - special function that allows us to add an item to a list
+
+var bookbag =['pencil','pen','notebook','fiction book']
+bookbag.push('sketch book')
+console.log(bookbag)
+
+//pop() special function that allows us to move an item from a list. the most recnet item, usually at the end of the list, will be removed
+bookbag.pop()
+console.log(bookbag)
